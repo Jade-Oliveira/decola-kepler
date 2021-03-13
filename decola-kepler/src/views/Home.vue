860213a2e4bd3c247ba22fe1871004b286a5683d
Jade Oliveira <jade.ufrgs@gmail.com>
2021-03-13 14:36:59 -0300

    inclusão da lista da api

--- decola-kepler/src/views/Home.vue
+++ decola-kepler/src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Lista/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Lista from '@/views/Lista.vue'//excluir antes de colocar no github junto com Lista em todos os lugares que inseri*
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Lista//excluir antes de colocar no github
   }
 }
 </script>
